9de7afc0654a11f66241637ed00eb544e5e425fe
khuzha <mr.khuzha@gmail.com>
2020-03-27 23:34:05 +0300

    remove kek

--- main.cpp
+++ main.cpp
@@ -12,13 +12,12 @@ int main()
   float sum = 1;
   float actNum = calc(x, 2);
 
-  cout << "kek";
   for (int degree = 4; actNum > e; degree += 2)
   {
     sum += actNum;
     actNum = calc(x, degree);
   }
 
-  cout << "Result (sum) = " << sum << endl;
+  cout << "Результат (sum) = " << sum << endl;
   return 0;
 }
